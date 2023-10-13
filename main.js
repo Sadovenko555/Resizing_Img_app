4cd9e9056cca064c4265e1ea50505f035b05a529
Ihor <sadovenko555@gmail.com>
2023-10-13 22:49:15 +0300

    fix

--- main.js
+++ main.js
@@ -20,7 +20,6 @@ function resizeImages() {
     const maxDimension = 1500;
     const progressBar = document.getElementById('progressBar');
     const imageCountElement = document.getElementById('imageCount');
-
     const zip = new JSZip();
     let processedImages = 0;
 
@@ -36,28 +35,36 @@ function resizeImages() {
                 let newWidth, newHeight;
 
                 if (image.width > image.height) {
-                    newWidth = maxDimension;
-                    newHeight = (image.height / image.width) * maxDimension;
+                    newWidth = image.width <= maxDimension ? image.width : maxDimension;
+                    newHeight = (image.height / image.width) * newWidth;
                 } else {
-                    newHeight = maxDimension;
-                    newWidth = (image.width / image.height) * maxDimension;
+                    newHeight = image.height <= maxDimension ? image.height : maxDimension;
+                    newWidth = (image.width / image.height) * newHeight;
                 }
 
                 const canvas = document.createElement('canvas');
                 const ctx = canvas.getContext('2d');
 
-                canvas.width = newWidth;
-                canvas.height = newHeight;
+                canvas.width = maxDimension;
+                canvas.height = maxDimension;
 
-                if (file.type === 'image/png' || file.type === 'image/webp') {
+              
+                if (file.type === 'image/png' && image.width <= maxDimension && image.height <= maxDimension) {
+                  
+                    ctx.clearRect(0, 0, maxDimension, maxDimension);
+                } else {
+                  
                     ctx.fillStyle = '#FFFFFF';
-                    ctx.fillRect(0, 0, newWidth, newHeight);
+                    ctx.fillRect(0, 0, maxDimension, maxDimension);
                 }
 
-                ctx.drawImage(image, 0, 0, newWidth, newHeight);
+             
+                const offsetX = (maxDimension - newWidth) / 2;
+                const offsetY = (maxDimension - newHeight) / 2;
+                ctx.drawImage(image, offsetX, offsetY, newWidth, newHeight);
 
                 canvas.toBlob(function (blob) {
-                    const fileName = file.name.replace(/\.[^/.]+$/, "");
+                    const fileName = file.name.replace(/\.[^/.]+$/, '');
                     const newFileName = fileName + '_resized.jpg';
 
                     zip.file(newFileName, blob);
@@ -68,8 +75,8 @@ function resizeImages() {
                     progressBar.style.width = `${progress}%`;
 
                     if (processedImages === files.length) {
-                        zip.generateAsync({ type: "blob" }).then(function (content) {
-                            saveAs(content, "resized_images.zip");
+                        zip.generateAsync({ type: 'blob' }).then(function (content) {
+                            saveAs(content, 'resized_images.zip');
                             progressBar.style.width = '0%';
                             imageCountElement.textContent = `Number of images: 0`;
                         });
@@ -81,6 +88,8 @@ function resizeImages() {
         reader.readAsDataURL(file);
     }
 }
+
+
 
 function handleFileSelect() {
     const input = document.getElementById('imageInput');
